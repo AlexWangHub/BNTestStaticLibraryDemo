f8acfd2b480d115733a8e68dc551a112be27c776
binbinwang <307649087@qq.com>
2021-08-14 17:23:00 +0800

    Initial Commit

---
+++ BNTestStaticLibraryDemo/BNTestStaticLibraryDemo.h
@@ -0,0 +1,18 @@
+//
+//  BNTestStaticLibraryDemo.h
+//  BNTestStaticLibraryDemo
+//
+//  Created by binbinwang on 2021/8/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BNTestStaticLibraryDemo.
+FOUNDATION_EXPORT double BNTestStaticLibraryDemoVersionNumber;
+
+//! Project version string for BNTestStaticLibraryDemo.
+FOUNDATION_EXPORT const unsigned char BNTestStaticLibraryDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BNTestStaticLibraryDemo/PublicHeader.h>
+
+
