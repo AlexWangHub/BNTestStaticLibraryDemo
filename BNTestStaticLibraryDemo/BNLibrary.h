4aca2ddbec708940c86f9afb1e415bfabdc59108
binbinwang <307649087@qq.com>
2021-08-15 01:42:55 +0800

    first commit

---
+++ BNTestStaticLibraryDemo/BNLibrary.h
@@ -0,0 +1,16 @@
+//
+//  BNLibrary.h
+//  BNTestStaticLibraryDemo
+//
+//  Created by binbinwang on 2021/8/14.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BNLibrary : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BNTestStaticLibraryDemo/BNLibrary.m
@@ -0,0 +1,12 @@
+//
+//  BNLibrary.m
+//  BNTestStaticLibraryDemo
+//
+//  Created by binbinwang on 2021/8/14.
+//
+
+#import "BNLibrary.h"
+
+@implementation BNLibrary
+
+@end
